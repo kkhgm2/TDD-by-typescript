0a116108734e0e38b90d832120a52684eb9760cd
kkhgm2 <pma225jkoi7265@yahoo.co.jp>
2021-03-22 23:33:00 +0900

    add red test add.test.ts

--- src/__tests__/add.test.ts
+++ src/__tests__/add.test.ts
@@ -1,6 +1,14 @@
 import { add } from '../script';
 
-test('over 1000', () => {
-  expect(add("add", "1", "1000")).toBe("too big");
-});
+describe("", () => {
 
+    test('計算結果が１０００を超える時', () => {
+        const args = ["add", "1", "1000"]
+        expect(add(args)).toBe("too big");
+    });
+    test('計算結果が１０００を超えない', () => {
+        const args = ["add", "1", "999"]
+        expect(add(args)).toBe("1000");
+    });
+
+});
--- src/script.ts
+++ src/script.ts
@@ -1,10 +1,12 @@
+import {checkArgsCount, checkArgsType} from "./common"
+
 const args = process.argv;
 
 console.log(args.slice(2))
 console.log(typeof args.slice(2,3))
 
 
-export const add = function  (...args: string[]): string {
+export const add = function  (args: string[]): string {
     
     return "too big";
 }
