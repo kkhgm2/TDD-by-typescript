b96e5039bb2d753ba3ff6318033af018d92db53f
kkhgm2 <pma225jkoi7265@yahoo.co.jp>
2021-03-25 23:18:38 +0900

    本実装

--- src/__tests__/add.test.ts
+++ src/__tests__/add.test.ts
@@ -1,13 +1,21 @@
-import { add } from '../script';
+import { add, hasFormula } from '../script';
 
 describe("", () => {
 
+    test("足し算が指定されている",() => {
+        const formula = "add"
+        expect(hasFormula(formula)).toBe(true);
+    });
+    test("足し算が指定されていない",() => {
+        const formula = "１０"
+        expect(hasFormula(formula)).toBe(false);
+    });
     test('計算結果が１０００を超える時', () => {
-        const args = ["add", "1", "1000"]
+        const args = ["1", "1000"]
         expect(add(args)).toBe("too big");
     });
     test('計算結果が１０００を超えない', () => {
-        const args = ["add", "1", "999"]
+        const args = ["1", "999"]
         expect(add(args)).toBe("1000");
     });
 
--- src/script.ts
+++ src/script.ts
@@ -1,12 +1,46 @@
-import {checkArgsCount, checkArgsType} from "./common"
+import {isArgsCountLess31, hasNumber} from "./common"
 
-const args = process.argv;
+let args: string[] = process.argv.slice(2);
+// console.log(args)
 
-console.log(args.slice(2))
-console.log(typeof args.slice(2,3))
+const formula: string = args[0];
+const targetNumber: string[] = args.slice(1);  
 
+// console.log(formula)
+// console.log(targetNumber)
 
+let errorMessage : string;
+
+//クラス化して、calculateの方がよさそう
 export const add = function  (args: string[]): string {
+    const callback = (a, b) => {
+        return a + parseInt(b);
+    }
+    let sum = args.reduce(callback, 0) 
     
-    return "too big";
+    if (sum >1000) {
+        return "too big";
+    }
+    return sum.toString();
+}
+
+// 　/$%& 辺りを入れると、バグる　マッチャーいれるか
+export const hasFormula = function (formula: string) : boolean {
+    const isAdd :boolean = "add" == formula;
+    const isMiuns :boolean = "minus" == formula;
+    const isMulti :boolean = "multiple" == formula;
+    const isDivide :boolean = "divide" == formula;
+
+    return isAdd || isMiuns || isMulti ||isDivide;
+}
+
+if (! hasFormula(formula))  errorMessage = "引数に計算方法を入力してください。"
+if (! isArgsCountLess31(args)) errorMessage = "引数が多すぎます。３０個までにしてください！";
+if (! hasNumber(targetNumber)) errorMessage = "計算する対象に、文字が含まれています！";
+
+let sum :string;
+if (! errorMessage) {
+    sum = add(targetNumber);
+} else {
+    console.log(errorMessage)
 }
