e1480fe9f5af370e988b960ea8a15384a93b812f
kkhgm2 <pma225jkoi7265@yahoo.co.jp>
2021-03-27 22:41:29 +0900

    本実装

--- src/script.ts
+++ src/script.ts
@@ -1,40 +1,49 @@
-import { Add } from "./add";
-import { Minus } from "./minus";
-import { Divide } from "./divide";
-import { Multiply } from "./multiply";
-
-import {Common} from "./common"
+import { Common } from "./common";
 import { Formula } from "./formula";
-
+import { Factory } from "./factory";
 
 let args: string[] = process.argv.slice(2);
 // console.log(args)
 
 const formula: string = args[0];
-const targetNumber: string[] = args.slice(1);  
+const targetNumber: string[] = args.slice(1);
 
 // console.log(formula)
 // console.log(targetNumber)
 
-let errorMessage : string;
-
-
+//共通処理、引数チェック
+let errorMessage: string;
 const common: Common = new Common();
+if (!common.hasFormula(formula))
+  errorMessage = "引数に計算方法を入力してください。";
+if (!common.isArgsCountLess31(args))
+  errorMessage = "引数が多すぎます。３０個までにしてください！";
+if (common.hasStorangeWord(args)) errorMessage = "不正な文字が入力されています";
+if (!common.hasNumber(targetNumber))
+  errorMessage = "計算する対象に、文字が含まれています！";
 
-if (! common.hasFormula(formula))  errorMessage = "引数に計算方法を入力してください。"
-if (! common.isArgsCountLess31(args)) errorMessage = "引数が多すぎます。３０個までにしてください！";
-if (common.hasStorangeWord(args)) errorMessage = "不正な文字が入力されています"
-if (! common.hasNumber(targetNumber)) errorMessage = "計算する対象に、文字が含まれています！";
+let usingFormula: Formula;
+switch (formula) {
+  case "add":
+    usingFormula = Factory.makeAddInstance();
+    break;
+  case "minus":
+    usingFormula = Factory.makeMinusInstance();
+    break;
+  case "multiply":
+    usingFormula = Factory.makeMultiplyInstance();
+    break;
+  case "divide":
+    usingFormula = Factory.makeDivideInstance();
+    break;
+}
 
-// const add:Formula = new Add();
+if (! usingFormula) errorMessage = "add, minus, multiply, divide の中から選択してください！！"; 
 
-const add:Formula = new Minus();
-const div:Formula = new Divide();
-
-let sum :string;
-if (! errorMessage) {
-    sum = div.calculate(targetNumber);
-    console.log(sum)
+let reslut: string;
+if (!errorMessage) {
+  reslut = usingFormula.calculate(targetNumber);
+  console.log(reslut);
 } else {
-    console.log(errorMessage)
+  console.log(errorMessage);
 }
